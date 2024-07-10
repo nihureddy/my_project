9265972825b4140ea865f21267b13f50f78e91d6
niharika.kotha <niharikakotha07@gmail.com>
2024-07-11 00:08:24 +0530

    add_sub

---
+++ first_program.py
@@ -0,0 +1,7 @@
+def addition(x, y):
+    return x+y
+
+a = int(input("enter a value "))
+b = int(input("enter b value "))
+z = addition(a,b)
+print("sum is ",z)
---
+++ second_program.py
@@ -0,0 +1,7 @@
+def subtraction(x,y):
+    return x-y
+
+a = int(input("enter a value "))
+b = int(input("enter b value "))
+z = subtraction(a,b)
+print("sub is ",z)
